2fef62fa2307b66920dd668c87e8cc68c9ad737a
Londzie <yardieyard@gmail.com>
2019-09-09 15:07:11 +0200

    fixes and features

--- admin/admin_nav.php
+++ admin/admin_nav.php
@@ -1,11 +1,114 @@
-<nav class="navbar fixed-bottom nav-pills nav-fill nav-justified navbar-dark bg-dark text-white">
-    <li class="nav-item">
-        <a class="nav-link active mx-2" href="assign.php">Assign</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link active mx-2" href="transactions.php">Transactions</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link active mx-2" href="users.php">Users</a>
-    </li>
-</nav>
+<?php
+include("auth.php");
+?>
+<!DOCTYPE html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Cash Bankers</title>
+
+    <!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+       <!--CUSTOM BASIC STYLES-->
+    <link href="assets/css/basic.css" rel="stylesheet" />
+    <!--CUSTOM MAIN STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+    <!-- GOOGLE FONTS-->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+    <div id="wrapper">
+        <nav class="navbar navbar-default navbar-cls-top " role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.php">Cash Bankers</a>
+            </div>
+
+            <div class="header-right">
+
+                <a href="message-task.html" class="btn btn-info" title="New Message"><b>30 </b><i class="fa fa-envelope-o fa-2x"></i></a>
+                <a href="message-task.html" class="btn btn-primary" title="New Task"><b>40 </b><i class="fa fa-bars fa-2x"></i></a>
+                <a href="admin_login.php" class="btn btn-danger" title="Logout"><i class="fa fa-exclamation-circle fa-2x"></i></a>
+
+            </div>
+        </nav>
+        <!-- /. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+					<li>
+                        <div class="user-img-div">
+                            <div class="user-img-div inner-text">
+                                <h4><i class="fa fa-sign-in "></i>   Administrator: <?php echo $_SESSION['username']; ?></h4>
+                            </div>
+                        </div>
+
+                    </li>
+                    <li>
+                        <a class="active-menu" href="index.php"><i class="fa fa-dashboard "></i>Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-desktop "></i>Settings <span class="fa arrow"></span></a>
+                         <ul class="nav nav-second-level">
+                            <li>
+                                <a href="panel-tabs.html"><i class="fa fa-toggle-on"></i>Tabs & Panels</a>
+                            </li>
+                            <li>
+                                <a href="notification.html"><i class="fa fa-bell "></i>Notifications</a>
+                            </li>
+                             <li>
+                                <a href="progress.html"><i class="fa fa-circle-o "></i>Progressbars</a>
+                            </li>
+                             <li>
+                                <a href="buttons.html"><i class="fa fa-code "></i>Buttons</a>
+                            </li>
+                             <li>
+                                <a href="icons.html"><i class="fa fa-bug "></i>Icons</a>
+                            </li>
+                             <li>
+                                <a href="wizard.html"><i class="fa fa-bug "></i>Wizard</a>
+                            </li>
+                             <li>
+                                <a href="typography.html"><i class="fa fa-edit "></i>Typography</a>
+                            </li>
+                             <li>
+                                <a href="grid.html"><i class="fa fa-eyedropper "></i>Grid</a>
+                            </li>
+                            
+                           
+                        </ul>
+                    </li>
+                     <li>
+                        <a href="#"><i class="fa fa-yelp "></i>Manage Users <span class="fa arrow"></span></a>
+                         <ul class="nav nav-second-level">
+                            <li>
+                                <a href="transactions.php"><i class="fa fa-coffee"></i>Transactions</a>
+                            </li>
+                            <li>
+                                <a href="pricing.html"><i class="fa fa-flash "></i>Profits</a>
+                            </li>
+                             <li>
+                                <a href="users.php"><i class="fa fa-key "></i>Users</a>
+                            </li>
+                            
+                             <li>
+                                <a href="assign.php"><i class="fa fa-recycle "></i>Assign Users</a>
+                            </li>
+                            
+                           
+                        </ul>
+                    </li>
+                </ul>
+
+            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
